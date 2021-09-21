4611cc767708338e8d8d0cb5ef7b66f19a73599b
Deovan Zanol <dzanol.atomic@alelo.com.br>
2021-09-21 19:49:22 -0300

    first commit

---
+++ src/br/com/dio/Primeiroprograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class Primeiroprograma {
+    public static void main( String[] args ) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+//        int b = 3;
+//        int a = 5;
+//        System.out.println( "Hello World! " + ( a + b ) );
+
+    }
+}
